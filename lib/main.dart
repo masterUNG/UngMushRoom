9c782b519a31677ff7039b70a50fb3fd9418e709
masterUNG <phrombutr@gmail.com>
2019-11-13 13:47:00 +0700

    Tab Controller Success

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ungmushroom/screens/home.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Ung MushRoom',
       home: Home(),
     );
--- lib/screens/my_service.dart
+++ lib/screens/my_service.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ungmushroom/utility/my_style.dart';
+import 'package:ungmushroom/widget/control.dart';
+import 'package:ungmushroom/widget/monitor.dart';
+import 'package:ungmushroom/widget/setting.dart';
 
 class MyService extends StatefulWidget {
   @override
@@ -6,12 +10,71 @@ class MyService extends StatefulWidget {
 }
 
 class _MyServiceState extends State<MyService> {
+  // Field
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  // Method
+  Widget hambargerButton() {
+    return IconButton(
+      icon: Icon(Icons.menu),
+      onPressed: () {
+        scaffoldKey.currentState.openDrawer();
+      },
+    );
+  }
+
+  Widget showDrawer() {
+    return Drawer();
+  }
+
+  Widget tabsMonitor() {
+    return Tab(
+      icon: Icon(Icons.airplay),
+      text: 'Monitor',
+    );
+  }
+
+  Widget tabsControl() {
+    return Tab(
+      icon: Icon(Icons.screen_lock_portrait),
+      text: 'Control',
+    );
+  }
+
+  Widget tabsSetting() {
+    return Tab(
+      icon: Icon(Icons.settings),
+      text: 'Setting',
+    );
+  }
+
+  Widget showTabController() {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: hambargerButton(),
+          backgroundColor: MyStyle().textColor,
+          title: Text('My Service'),
+          bottom: TabBar(indicatorColor: Colors.white,
+            indicatorWeight: 4.0,
+            tabs: <Widget>[
+              tabsMonitor(),
+              tabsControl(),
+              tabsSetting(),
+            ],
+          ),
+        ),body: TabBarView(children: <Widget>[Monitor(), Control(), Setting(),],),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Service'),
-      ),body: Text('body'),
+      key: scaffoldKey,
+      body: showTabController(),
+      drawer: showDrawer(),
     );
   }
 }
